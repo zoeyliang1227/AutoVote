99ecf58d014e8278690f73aa7098b455ae0bc71d
zoeyliang <zoey.liang@pilottv.com.tw>
2021-10-30 01:05:25 +0800

    rapair

--- ro.py
+++ ro.py
@@ -5,6 +5,7 @@ import datetime
 import random
 import getpass
 import sys, os, time
+import random
 
 
 from threading import Timer
@@ -16,20 +17,41 @@ from selenium.webdriver.support import expected_conditions as EC
 
 credentials = yaml.load(open('config.yml'))
 
-def job():
-    print("I'm working...")
+hour = random.randint(7,12)
+minute = random.randint(0,59)
+
+def doSth():
     ro()
+    print(u'這個程式要開始瘋狂的運轉啦')
+
+def main(h=hour,m=minute):
+    print(h,m)
+    while True:
+        now = datetime.datetime.now()
+        # print(now.hour, now.minute)
+        if now.hour == h and now.minute == m:
+            break
+    
+    doSth()
+
+    # 每86400秒（1天），傳送1次
+    t = Timer(86400, main)
+    t.start()
+
+# def job():
+#     print("I'm working...")
+#     ro()
 
 # schedule.every(10).minutes.do(job) 
 # schedule.every().hour.do(job)
-schedule.every().day.at('10:11').do(job)
+# schedule.every().day.at('10:14').do(job)
 # schedule.every().monday.do(job)
 # schedule.every().wednesday.at("13:15").do(job)
 # schedule.every().minute.at(":17").do(job)   
 
-while True:  
-    schedule.run_pending()  
-    time.sleep(1)
+# while True:  
+#     schedule.run_pending()  
+#     time.sleep(1)
 
 def get_driver():
     chrome_options = Options()
@@ -45,7 +67,7 @@ def get_driver():
     chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
     chrome_options.add_argument("disable-infobars")
     chrome_options.add_argument("window-size=1024,768")
-    # chrome_options.add_argument('headless')                 # 瀏覽器不提供可視化頁面
+    chrome_options.add_argument('headless')                 # 瀏覽器不提供可視化頁面
     chrome_options.add_argument('no-sandbox')               # 以最高權限運行
     chrome_options.add_argument('--start-maximized')        # 縮放縮放（全屏窗口）設置元素比較準確
     chrome_options.add_argument('--disable-gpu')            # 谷歌文檔說明需要加上這個屬性來規避bug
@@ -65,12 +87,13 @@ def get_driver():
 
 def ro():    
     try:
-        for i in range(1,5):
+        for i in range(1, 17):
             driver = get_driver()
             # wait.until(EC.presence_of_element_located((By.CLASS_NAME, 'style-module__title--1GEE0')))
             vote(driver)
-            login(i, driver)
             print(i)
+            time.sleep(3)
+            login(i, driver)            
             driver.refresh()
             time.sleep(3)
             vote(driver)
@@ -80,7 +103,6 @@ def ro():
             done_title = done_vote.text
             if done_title == '作品當天已投過票':
                 print(done_title)
-                # close = driver.find_element(By.XPATH, '//*[@id="useModal__3"]/div/div/div[1]/div[2]').click()
                 # driver.refresh()
                 # time.sleep(3)
                 # logout = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[1]/div[2]/div[2]/span')
@@ -91,12 +113,18 @@ def ro():
             else:
                 print(done_title)  
                 print('太感謝你了٩(●˙▿˙●)۶…⋆ฺ')
+                get_vote = driver.find_element(By.XPATH, '//*[@id="root"]/div[1]/div[4]/div[1]/div[4]/div/div[1]/div/div[6]/div[1]/span')
+                votes = get_vote.text
+                print('目前票數%s'%votes)
+
                 driver.quit()
+                r = random.randrange(1,180)
+                time.sleep(r)
     finally:
         time.sleep(3)
         driver.quit()
 
-def vote(driver):     #choose and vote
+def vote(driver):     
     choose = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[4]/div[1]/div[2]/div[2]')
     choose.click()
     WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.CLASS_NAME, 'style-module__active--1YiWY')))
@@ -104,8 +132,9 @@ def vote(driver):     #choose and vote
     print('click %s'%choose_title)
 
     time.sleep(3)
-    vote = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[4]/div[1]/div[4]/div/div[1]/div/div[5]').click()
-    time.sleep(3)
+    vote = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[4]/div[1]/div[4]/div/div[1]/div/div[5]')
+    WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.CLASS_NAME, 'style-module__title--2JLqT')))
+    vote.click()
     
 def login(i, driver):
     WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.CLASS_NAME, 'style-module__title--1v3XO')))
@@ -118,6 +147,7 @@ def login(i, driver):
     time.sleep(3)
     login = driver.find_element(By.ID, 'loginbutton').click()
     print('%s登入成功'%username)
+    time.sleep(3)
 
     return i
 
@@ -127,4 +157,4 @@ if __name__ == '__main__':
     # input_id = input('Your FB ID:')
     # input_password = getpass.getpass('Your FB password') 
     # ro()
-    job()
+    main()
